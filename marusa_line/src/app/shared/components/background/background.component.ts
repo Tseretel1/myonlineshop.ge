82114268294b7dfc95e0e05e1d39866ae5912a15
George <giorgitsereteli541@gmail.com>
2026-03-17 12:22:57 +0400

    modified background of the app, modified orders

--- marusa_line/src/app/shared/components/background/background.component.ts
+++ marusa_line/src/app/shared/components/background/background.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
 @Component({
   selector: 'app-background',
   imports: [],
   templateUrl: './background.component.html',
   styleUrl: './background.component.scss'
 })
-export class BackgroundComponent {
+export class BackgroundComponent  {
 
 }
